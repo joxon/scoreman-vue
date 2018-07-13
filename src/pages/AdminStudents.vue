e91925d2ea844adfb5fd5c1353e89792272a0e63
Joxon <joxonchan@qq.com>
2018-07-14 00:12:58 +0800

    更新admin各页面布局，按钮逻辑待完成

--- src/pages/AdminStudents.vue
+++ src/pages/AdminStudents.vue
@@ -1,22 +1,26 @@
 <template>
-  <el-table :data="tableData" stripe>
-    <el-table-column prop="sid" label="学生号" width="70"/>
-    <el-table-column prop="sname" label="姓名" width="70"/>
-    <el-table-column prop="classno" label="班级" width="100"/>
-    <el-table-column prop="sex" label="性别" width="50"/>
-    <el-table-column prop="password" label="密码" />
-    <el-table-column label="操作" width="150">
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <el-container direction='vertical'>
+    <el-button-group>
+      <el-button type='success' icon='el-icon-plus' @click="addOneStudent()">新增学生</el-button>
+      <el-button type='danger' icon='el-icon-minus' @click="deleteSelections()">删除所选</el-button>
+    </el-button-group>
+
+    <el-table :data="tableData" stripe height="250">
+      <el-table-column type="selection" width="40" />
+      <el-table-column sortable prop="sid" label="学生号" width="90" />
+      <el-table-column prop="sname" label="姓名" width="70" />
+      <el-table-column prop="classno" label="班级" width="100" />
+      <el-table-column prop="sex" label="性别" width="50" />
+      <el-table-column prop="password" label="密码" />
+      <el-table-column label="操作" width="150">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+  </el-container>
 </template>
 
 <script>
@@ -34,6 +38,55 @@
       return {
         tableData: [{
             sid: "S0001",
+            sname: "王小虎",
+            classno: "计科十班",
+            sex: "男",
+            password: "123"
+          },
+          {
+            sid: "S0002",
+            sname: "王小虎",
+            classno: "计科十班",
+            sex: "男",
+            password: "123"
+          },
+          {
+            sid: "S0003",
+            sname: "王小虎",
+            classno: "计科十班",
+            sex: "男",
+            password: "123"
+          },
+          {
+            sid: "S0004",
+            sname: "王小虎",
+            classno: "计科十班",
+            sex: "男",
+            password: "123"
+          },
+          {
+            sid: "S0005",
+            sname: "王小虎",
+            classno: "计科十班",
+            sex: "男",
+            password: "123"
+          },
+          {
+            sid: "S0001",
+            sname: "王小虎",
+            classno: "计科十班",
+            sex: "男",
+            password: "123"
+          },
+          {
+            sid: "S0002",
+            sname: "王小虎",
+            classno: "计科十班",
+            sex: "男",
+            password: "123"
+          },
+          {
+            sid: "S0003",
             sname: "王小虎",
             classno: "计科十班",
             sex: "男",
--- src/pages/AdminTeachers.vue
+++ src/pages/AdminTeachers.vue
@@ -1,47 +1,49 @@
 <template>
-  <el-table :data="tableData" stripe>
-    <el-table-column prop="tid" label="教师号" width="70"/>
-    <el-table-column prop="tname" label="姓名" width="70"/>
-    <el-table-column prop="sex" label="性别" width="50"/>
-    <el-table-column prop="email" label="邮箱" width="150"/>
-    <el-table-column prop="password" label="密码" />
-    <el-table-column label="操作" width="150">
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <el-container direction='vertical'>
+    <el-button-group>
+      <el-button type='success' icon='el-icon-plus' @click="addOneCourse()">新增教师</el-button>
+      <el-button type='danger' icon='el-icon-minus' @click="deleteSelections()">删除所选</el-button>
+    </el-button-group>
+
+    <el-table :data="tableData" stripe>
+      <el-table-column type="selection" width="40" />
+      <el-table-column prop="tid" label="教师号" width="70" />
+      <el-table-column prop="tname" label="姓名" width="70" />
+      <el-table-column prop="sex" label="性别" width="50" />
+      <el-table-column prop="email" label="邮箱" width="150" />
+      <el-table-column prop="password" label="密码" />
+      <el-table-column label="操作" width="150">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-container>
 </template>
 
 <script>
-export default {
-  methods: {
-    handleEdit(index, row) {
-      console.log(index, row);
+  export default {
+    methods: {
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      }
     },
-    handleDelete(index, row) {
-      console.log(index, row);
-    }
-  },
 
-  data() {
-    return {
-      tableData: [
-        {
+    data() {
+      return {
+        tableData: [{
           tid: "T0001",
           tname: "杜君鹏",
           email: "djp@bupt.edu.cn",
           sex: "男",
           password: "123"
-        }
-      ]
-    };
-  }
-};
+        }]
+      };
+    }
+  };
+
 </script>
