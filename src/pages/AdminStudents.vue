974fb898c761521f7785b6e44bef8beb46d9af0f
Joxon <joxonchan@qq.com>
2018-07-14 19:58:56 +0800

    管理员学生页面全部完成。

--- src/pages/AdminStudents.vue
+++ src/pages/AdminStudents.vue
@@ -3,40 +3,41 @@
     <el-button-group>
       <el-button type='primary' icon='el-icon-refresh' @click='getStudents()'>刷新</el-button>
       <el-button type='success' icon='el-icon-plus' @click='showAddStuForm()'>新增学生</el-button>
-      <el-button type='danger' icon='el-icon-minus' @click='deleteSelections()'>删除所选</el-button>
+      <el-button type='danger' icon='el-icon-minus' @click='handleDeleteSels()'>删除所选</el-button>
     </el-button-group>
 
     <el-dialog :title='stuFormTitle' :visible.sync='stuFormVisible'>
-      <el-form ref='stuForm' :rules='rules' :model='stuFormModel'>
-        <el-form-item prop='sID' label='学生号' :label-width='formLabelWidth'>
-          <el-input :disabled="sIDInputDisabled" v-model='stuFormModel.sID' auto-complete='off'>
-            <template slot="prepend">学生号开头默认加S</template>
+      <el-form ref='stuForm' :rules='stuFormRules' :model='stuFormModel' label-width="80px">
+        <el-form-item prop='sID' label='学生号' >
+          <el-input :disabled="sIDInputDisabled" v-model='stuFormModel.sID'>
+            <template slot="prepend">S</template>
           </el-input>
         </el-form-item>
-        <el-form-item prop='sName' label='姓名' :label-width='formLabelWidth'>
-          <el-input v-model='stuFormModel.sName' auto-complete='off'></el-input>
+        <el-form-item prop='sName' label='姓名' >
+          <el-input v-model='stuFormModel.sName'></el-input>
         </el-form-item>
-        <el-form-item prop='classno' label='班级' :label-width='formLabelWidth'>
-          <el-input v-model='stuFormModel.classno' auto-complete='on'></el-input>
+        <el-form-item prop='classno' label='班级' >
+          <el-input v-model='stuFormModel.classno'></el-input>
         </el-form-item>
-        <el-form-item prop='sex' label='性别' :label-width='formLabelWidth'>
+        <el-form-item prop='sex' label='性别' >
           <el-select v-model='stuFormModel.sex' placeholder='请选择性别'>
             <el-option label='男' value='男'></el-option>
             <el-option label='女' value='女'></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item prop='password' label='密码' :label-width='formLabelWidth'>
-          <el-input v-model='stuFormModel.password' auto-complete='off' type='password'></el-input>
+        <el-form-item prop='password' label='密码' >
+          <el-input v-model='stuFormModel.password' type='password'></el-input>
         </el-form-item>
       </el-form>
       <div slot='footer'>
+        <el-button @click='resetStuForm()'>清空</el-button>
         <el-button @click='hideStuForm()'>取消</el-button>
         <el-button v-if='addButtonVisible' type='primary' @click='handleAdd()'>确定添加</el-button>
         <el-button v-if='edtButtonVisible' type='primary' @click='handleEditFromForm()'>确定编辑</el-button>
       </div>
     </el-dialog>
 
-    <el-table :data='stuTableData' stripe>
+    <el-table :data='stuTableData' stripe max-height='500' @selection-change="handleSelectionChange">
       <el-table-column type='selection' width='40' />
       <el-table-column sortable prop='sID' label='学生号' width='90' />
       <el-table-column prop='sName' label='姓名' width='70' />
@@ -46,7 +47,7 @@
       <el-table-column fixed='right' label='操作' width='200'>
         <template slot-scope='scope'>
           <el-button icon='el-icon-edit' size='mini' @click='handleEditFromMenu(scope.$index, scope.row)'>编辑</el-button>
-          <el-button icon='el-icon-delete' size='mini' type='danger' @click='handleDelete(scope.$index, scope.row)'>删除</el-button>
+          <el-button icon='el-icon-delete' size='mini' type='danger' @click='handleDeleteFromMenu(scope.$index, scope.row)'>删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -58,6 +59,7 @@
 export default {
   data() {
     return {
+      selectedStudents: [],
       stuTableData: [
         {
           sID: "加载中",
@@ -67,7 +69,7 @@ export default {
           password: "加载中"
         }
       ],
-      rules: {
+      stuFormRules: {
         sID: [
           {
             required: true,
@@ -115,8 +117,7 @@ export default {
         classno: "",
         sex: "",
         password: ""
-      },
-      formLabelWidth: "80px"
+      }
     };
   },
 
@@ -125,6 +126,11 @@ export default {
   },
 
   methods: {
+    resetStuForm() {
+      this.resetStuFormModel();
+      this.$refs.stuForm.resetFields();
+    },
+
     hideStuForm() {
       this.stuFormVisible = false;
     },
@@ -145,7 +151,7 @@ export default {
       this.stuFormVisible = true;
     },
 
-    resetFormModel() {
+    resetStuFormModel() {
       this.stuFormModel = {
         sID: "",
         sName: "",
@@ -196,7 +202,7 @@ export default {
               center: true
             });
             this.getStudents();
-            this.resetFormModel();
+            this.resetStuFormModel();
           } else {
             this.$message({
               type: "error",
@@ -246,7 +252,7 @@ export default {
               center: true
             });
             this.getStudents();
-            this.resetFormModel();
+            this.resetStuFormModel();
           } else {
             this.$message({
               type: "error",
@@ -267,7 +273,17 @@ export default {
         });
     },
 
-    handleDelete(index, row) {
+    handleSelectionChange(selections) {
+      this.selectedStudents = selections;
+    },
+
+    handleDeleteSels() {
+      this.selectedStudents.forEach(student => {
+        this.delStudent(student.sID);
+      });
+    },
+
+    handleDeleteFromMenu(index, row) {
       this.$confirm("确定删除" + row.sID + "？", "警告", {
         confirmButtonText: "确定删除",
         cancelButtonText: "取消",
